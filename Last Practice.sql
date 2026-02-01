f2f2d4147aa0a4375a24e277663a8fa1da7826d3
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-02-01 19:06:28 +0600

    add

--- Last Practice.sql
+++ Last Practice.sql
@@ -41,3 +41,43 @@ SELECT
     * FROM  emp129400; 
     
 DESC   emp129400; 
+
+
+
+--Question-01
+create table emp1294101( 
+e_id number(10),
+e_name varchar2(26),
+salary varchar2(30),
+hire_date date,
+address varchar2(60),
+constraint demo_emp1294101_e_id_pk PRIMARY KEY (e_id)
+);
+
+--Question-02
+alter table emp1294101
+add (department_id varchar2(30) ,dept_name varchar(33));
+
+
+--Question-03
+INSERT INTO emp1294101(e_id,e_name,salary,hire_date,address,department_id,dept_name)
+values('1','A','1000','01-jan-2000','10/3A','101','Bangla');
+
+--Question-04
+create view emp129
+as select e_name,dept_name,salary 
+from emp1294101
+where department_id = 80;
+
+
+--Question-05
+create SEQUENCE emp129_e_id_sq
+START with 1
+INCREMENT BY 10 
+MAXVALUE 999999;
+
+
+--Question-06
+UPDATE emp1294101 set (e_name,salary)=(SELECT last_name,salary FROM employees
+where employee_id = 108)
+where e_id=1;
